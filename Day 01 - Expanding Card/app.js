9d35dae43def7eafb996b1dd738d68b5d09de55d
Osuohia Emmanuel <josephe442@gmail.com>
2021-12-01 22:11:04 +0100

    Refactoring

--- Day 01 - Expanding Card/app.js
+++ Day 01 - Expanding Card/app.js
@@ -1,14 +1,20 @@
+// getting the panels from html to js
 const panels = document.querySelectorAll('.panel')
 
+// looping through the panel with forEach to add an EventListener when clicked
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
+    // calling the removeActiveClasses function
     removeActiveClasses()
+    // added the class active
     panel.classList.add('active')
   })
 })
 
+// create the removeActiveClasses function
 const removeActiveClasses = () => {
   panels.forEach((panel) => {
+    // remove the class active
     panel.classList.remove('active')
   })
 }
